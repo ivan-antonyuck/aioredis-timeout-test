44440b4198d9321a6e5e79460b4060b6324775c3
Ivan Antonyuck <ia@axiros.com>
2021-04-22 14:41:32 +0300

    initial

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+__author__ = u'antonyuck'
+import aioredis
+import asyncio
+async def test():
+    key = 'test'
+    redis = aioredis.Redis(connection_pool=aioredis.ConnectionPool.from_url("redis://localhost", socket_timeout=1))
+    await redis.get('key')
+
+asyncio.get_event_loop().run_until_complete(test())
